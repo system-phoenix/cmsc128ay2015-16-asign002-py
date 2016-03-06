68301e664a7e674ec6ad705d8f0c43b79332b723
system-phoenix <dmorias@up.edu.ph>
2016-03-06 14:55:29 +0800

    Added test cases for isValidString function.

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ print("getHammingDistance(A, AG): ", bioLibrary.getHammingDistance("A", "AG"))
 print("countSubstrPattern(AATATATAGG, GG): ", bioLibrary.countSubstrPattern("AATATATAGG", "GG"))
 print("countSubstrPattern(AATATATAGG, ATA): ", bioLibrary.countSubstrPattern("AATATATAGG", "ATA"))
 print("countSubstrPattern(AATATATAGG, ACTGACTGACTG): ", bioLibrary.countSubstrPattern("AATATATAGG", "ACTGACTGACTG"))
+
+#Test cases for isValidString
+print("isValidString(AAGGCTATGC, ACGT): ", bioLibrary.isValidString("AAGGCTATGC", "ACGT"))
+print("isValidString(AAGGCTATGa, ACGT): ", bioLibrary.isValidString("AAGGCTATGa", "ACGT"))
+print("isValidString(ACGT, ACGT): ", bioLibrary.isValidString("ACGT", "ACGT"))
+print("isValidString(ACGT101_, ACGT): ", bioLibrary.isValidString("ACGT101_", "ACGT"))
+print("isValidString(091212345, 0123456789): ", bioLibrary.isValidString("091212345", "0123456789"))
